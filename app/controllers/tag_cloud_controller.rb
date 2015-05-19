a10e4dc77194e2c31912b1c5f15add90ff6fa666
arynchoong <arynchoong@hotmail.com>
2015-05-19 20:44:04 +0800

    Added definitions

--- app/controllers/tag_cloud_controller.rb
+++ app/controllers/tag_cloud_controller.rb
@@ -2,4 +2,22 @@ class TagCloudController < ApplicationController
   def index
     #We're changing this!
   end
+  
+  def show
+    if LoginConcern.user(params[:user_id]).is_signed_in
+      render Tag.find(params[:id]).count
+    else
+      redirect_to_root
+    end
+  end
+  
+  def create
+  end
+  
+  def update
+  end
+  
+  def destroy
+    
+  end
 end
